f46d11eeef7d5ba86a9dbccda41b280f94f05a86
HotDeal Developer <dev@hotdeal.local>
2026-02-14 12:00:19 +0900

    자동 커밋: 변경사항 반영

--- web_project/review_rpa/connectors/__init__.py
+++
@@ -1,6 +0,0 @@
-from connectors.unified import UnifiedReviewConnector
-
-
-def get_connector_class(_platform_name: str):
-    """요청사항: 플랫폼별 개별 파일 대신 단일 통합 커넥터 사용."""
-    return UnifiedReviewConnector
